3e739881da73cafa38df250afc1c9398ec417466
jiali0411 <jyu28@vols.utk.edu>
2021-07-23 15:05:52 -0500

    add AA properties

--- codes/MSA_embedding.py
+++ codes/MSA_embedding.py
@@ -4,15 +4,27 @@ Author: Jiali
 Usage: Take the HHBlits output hhm format, and encode the protein sequence
 python3 environment
 Run:
-python MSA_embedding.py <proteinID.hhm> <pdb.dssp.txt> <output txt file for embedding vector>
+python MSA_embedding.py <proteinID.hhm> <pdb.dssp.txt> <matrix_file.csv> <output txt file for embedding vector>
 """
 import sys
 import math
 import numpy as np
+import pandas as pd
 
 hhm_file = sys.argv[1]
 dssp_file = sys.argv[2]
-output_file = sys.argv[3]
+matrix_file = sys.argv[3]
+output_file = sys.argv[4]
+
+# add amino acid properties matrix to the final vector
+## read the matrix table and turn it into a dictionary
+### convert sequence to number vector
+AA_table = pd.read_csv(matrix_file)
+AA_array = AA_table.set_index('ID').T.to_dict('list')
+### convert the list in dictionary to array
+for key, value in AA_array.items():
+    value = list(map(str,value))
+    AA_array[key] = value
 
 with open(hhm_file) as f, open(dssp_file) as df, open(output_file ,"w") as out:
     for i in f:
@@ -38,6 +50,8 @@ with open(hhm_file) as f, open(dssp_file) as df, open(output_file ,"w") as out:
             hhm_vector = []
             content = line.split()
             hhm_vector = hhm_vector + content[:2] + [content[3]] # extract sequence AA and accessible surface area from dssp.txt
+            property_m = AA_array[content[1]] # assign each AA with its own property matrix, adopted from HDMD
+            hhm_vector = hhm_vector + property_m # AA property features
             for val1 in content1[2:-1]:
                 hhm_val1 = 10/(1 + math.exp(-1 * int(val1)/2000))
                 hhm_vector.append(str(hhm_val1))
