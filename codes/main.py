8ac9dcf54eafee529d872b36880459ad417c7804
zachw1 <87206585+zachw1@users.noreply.github.com>
2021-08-11 15:45:07 -0500

    Add files via upload
    running main.py with all the other files will produce a results file.

---
+++ codes/main.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from data_approximations import linreg
+from protein_merge import protein_merge
+
+manager = open("manager.txt", "r")
+
+for row in manager: 
+    files = row.split(": ")
+
+    df = pd.read_csv(files[0] +".csv")
+
+    #maxuptake
+    mut = []
+    if "Maxuptake" not in df[0:0]:
+        for index, row in df.iterrows():
+            mut.append(len(str((row["Sequence"]))) - 1 - str(row["Sequence"]).count("P"))
+    else:
+        for index, row in df.iterrows():
+            mut.append(row["Maxuptake"])
+
+    #uptake prediction
+    xlist, ylist, predict, predictedvals = [], [], 1.25, []
+    for index, row in df.iterrows():
+        xlist.append(row["Exposure"])
+        ylist.append(row["Uptake"])
+    predictedvals = linreg(xlist, ylist, predict)
+
+    #protein merge
+    start, end, sequence = [], [], []
+    for index, row in df.iterrows():
+        start.append(row["Start"])
+        end.append(row["End"])
+        sequence.append(row["Sequence"])
+    print(protein_merge(start, end, sequence))
+
+    i = 0; j = []
+    for index, row in df.iterrows():
+        
+        if row["State"] == files[1].strip():
+            j.append([row["Start"], row["End"], row["Sequence"], row["State"], mut[i], row["Uptake"], row["Exposure"]])
+        
+            pd.DataFrame(j, index = None, columns = ["Start", "End", "Sequence","State", "Maxuptake", "Uptake", "Exposure"]).to_csv(files[0] + "results.csv")
+        i+=1
+
+
+
