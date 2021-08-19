84cb99a854b1a77e5d6afe6e632b7c455f808ba6
zachw1 <87206585+zachw1@users.noreply.github.com>
2021-08-19 02:07:57 -0500

    Update data_approximations.py

--- codes/data_approximations.py
+++ codes/data_approximations.py
@@ -9,10 +9,15 @@ from sklearn.linear_model import LinearRegression
 
 def linreg(xlist, ylist, predict): #xlist are exposure values, ylist are uptake values
     predictedvals = []
-    for i in range(len(xlist)):
-        if xlist[i] == predict:
-            predictedvals.append(xlist[i])
-        else:
-            predictedvals.append( ( (ylist[i] - ylist[i-1])/(xlist[i] - xlist[i-1]) ) * (predict - xlist[i-1]) + ylist[i-1])
+    for i in range(len(xlist)-1):
+        if xlist[i] <= predict and xlist[i+1] > predict:
+            if xlist[i] == predict:
+                predictedvals.append(ylist[i])            
+            else:
+                try:
+                    predictedvals.append( ( (ylist[i+1] - ylist[i])/(xlist[i+1] - xlist[i]) ) * (predict - xlist[i]) + ylist[i])
+                except ZeroDivisionError:
+                    predictedvals.append("ZeroDivisionError")
     return predictedvals
+
     
