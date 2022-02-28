8c9b4099ad32718ac62da0c008cba26cd0966fdc
jiali0411 <jyu28@vols.utk.edu>
2022-02-28 15:07:19 -0600

    add new R code

---
+++ Rscripts/stepRes plot.R
@@ -0,0 +1,27 @@
+setwd("~/Desktop/Jiali/TAMU/Dynamics/Seq2HDX/Rscripts/")
+
+# read data file
+pred = read.csv("../results/stepRes XYN pred.xlsx - Sheet1.csv", header = T)
+
+# calculate mean and SE for each fragment
+library(Rmisc)
+library(reshape2)
+
+meltdf <- melt(pred[,c(1,6:15)], id.vars = "X")
+datasum <- summarySE(meltdf, measurevar = "value", groupvars = "X")
+
+datasum$col <- 1
+datasum$X <- datasum$X + 1
+
+library(ggplot2)
+ggplot(datasum, aes(x = col, y = X, fill= value))+
+  geom_tile()+
+  geom_text(aes(label=round(value, 2)))+
+  scale_fill_gradient(low="white",high="blue")+
+  theme_classic()+
+  scale_y_continuous(trans = "reverse", breaks = datasum$X)+
+  theme(axis.title.x = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text.x = element_blank())+
+  labs(x= "", y = "")
+ggsave("../images/stepRes heatmap.pdf", width = 2, height = 8)  
