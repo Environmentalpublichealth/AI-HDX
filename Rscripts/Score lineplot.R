edf397826f7a28b73f4b21b875ea59d2126b8e56
jiali0411 <jyu28@vols.utk.edu>
2022-03-28 10:33:25 -0500

    update

--- Rscripts/Score lineplot.R
+++ Rscripts/Score lineplot.R
@@ -6,7 +6,7 @@ df_data <- read.csv("../results/1xyn_alphaFold_conf.csv", header = T)
 library(reshape2)
 library(ggplot2)
 
-df_conf <- df_data[,c(1,5,6)]
+df_conf <- df_data[,c(1,2,5,6)]
 
 df_plot <- melt(df_conf, id = "peptides")
 
@@ -17,5 +17,9 @@ ggplot(df_plot, aes(x = peptides, y = value, group = variable, color = variable)
   geom_line(stat = "identity", aes(color = variable))+
   geom_point()+
   theme_classic()+
-  scale_color_discrete(name = "", labels = c("AlphaFold","Error"))+
+  scale_color_manual(values = c("#0072B2",'#66CC99', 'black'), name = "", labels = c("AI-HDX","AlphaFold","Error"))+
   labs(y = "Score")
+ggsave("../images/AlphaFold Conf.pdf", height = 2.5, width = 9)
+
+# confidence index vs error
+
